5c57856617feba4a0879da9e979d55665d10e24f
TanmayTR-200 <tanmaytr05@gmail.com>
2026-09-22 12:27:29 +0530

    Remove stale logs, local launcher scripts, and outdated 15-Sep TCN checkpoint (superseded by shipped tcn.keras)

--- New Models/run_gcn.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-python -u GCN\train_gcn.py ..\..\X_195_75.npy ..\..\y_195_75.npy > gcn_wmi.log 2>&1
-echo EXITCODE %ERRORLEVEL% > gcn_done.marker
--- New Models/run_tcn.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-python -u TCN\train_tcn.py ..\..\X_195_75.npy ..\..\y_195_75.npy > tcn_wmi.log 2>&1
-echo EXITCODE %ERRORLEVEL% > tcn_done.marker
